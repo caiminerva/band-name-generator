291df875631dbf8c1b9b6dcd4fcd41bf72f8081e
John Cairo Minerva <80746624+caiminerva@users.noreply.github.com>
2022-06-28 13:35:44 +0800

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Welcome to Band Name Generator \n")
+
+city = input("What City did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+band_name = print("\nYour band name could be " + city + " " + pet)
+
+
+# In[ ]:
+
+
+
+
